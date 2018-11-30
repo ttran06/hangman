00df9dd38f2d5cee66eb3465f2b11f448ddf69b6
Truc <ttran9754@gmail.com>
2018-11-30 12:07:15 -0500

    first commit

---
+++ hangman.py
@@ -0,0 +1,38 @@
+from graphics import *
+from time import sleep
+
+# TODO: Splash Window
+welcomeScreen =  GraphWin("Welcome", 400, 500)
+c = Circle(Point(200,200), 20)
+c.draw(welcomeScreen)
+sleep(3)
+welcomeScreen.close()
+# TODO: Category symbol graphics
+
+# TODO: COnnect category graphics to choice of words in the game
+
+# TODO: Create the guess Window
+
+# TODO: Create the components that are displayed on the window
+
+# TODO: Create guess window
+
+# TODO: Randomly select a word from correct file
+
+# TODO: Create dashes fom len(word)
+
+# TODO: User input window
+
+# TODO: Swinging of man animation
+
+# TODO: Determine if guessed letter is right or wrong
+
+# TODO: Put incorrect guess in TRASH
+
+# TODO: Put correct guess in dashes
+
+# TODO: Winner Splash screen
+
+# TODO: Loser Splash screen
+
+# TODO: Continue screen
