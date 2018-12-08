037bac8123d4f3feb33052fec720ecdc327e3191
Truc <ttran9754@gmail.com>
2018-12-08 15:24:32 -0500

    added category screen

--- hangman.py
+++ hangman.py
@@ -29,7 +29,7 @@ class Hangman:
             self.letter_list.append(self.word[i])
         for letter in self.word:
             if letter == " ":
-                print(" ", end=" ") # put space if character is space
+                print(" ", end=" ")  # put space if character is space
             print("_", end=" ")
 
     def take_guess_by_letter(self, guess_letter):
@@ -64,10 +64,11 @@ class Hangman:
 
         # print out comments on user's guesses
 
-        if (letter_guess == True):
-            print("\nYou guess on letter " + guess_letter + " is right!" )
+        if (letter_guess is True):
+            print("\nYou guess on letter " + guess_letter + " is right!")
         else:
             print("\nSorry ;/ The letter " + guess_letter + "doesn't occur in this word...")
+
     def take_guess_by_word(self, guess_word):
         guess = guess_word.upper()
         self.guessed_letters.append(guess)
@@ -100,6 +101,7 @@ class enterButton(Button):
         else:
             Hangman.take_guess_by_letter()
 
+
 def welcomeScreen():
     """Create the welcome screen and display it for 3 seconds."""
     welcomeScreen = GraphWin("Welcome", 800, 400)
@@ -107,6 +109,179 @@ def welcomeScreen():
     welcomeImage.draw(welcomeScreen)
     sleep(3)    # Open window for 3 seconds
     welcomeScreen.close()
+
+
+# This function Prints the categories.
+def CategoryScreen():
+    # Create window
+    win = GraphWin("Hangman", 1640, 800)
+
+    # creating the graphics
+    PickCategory = Image(Point(800, 100), "pickcategory.gif")
+    food = Image(Point(134, 500), "food.gif")
+    animal = Image(Point(455.5, 500), "animal2.gif")
+    countries = Image(Point(838, 500), "animal.gif")
+    Popculture = Image(Point(1243, 500), "Pop culture.gif")
+    questionmark = Image(Point(1545.5, 500), "questionmark.gif")
+
+    # Returns the width of the image
+    Width_food = food.getWidth()
+    Width_animal = animal.getWidth()
+    Width_countries = countries.getWidth()
+    Width_Popculture = Popculture.getWidth()
+    Width_questionmark = questionmark.getWidth()
+
+    # Returns the height of the image
+    Height_food = food.getHeight()
+    Height_animal = animal.getHeight()
+    Height_countries = countries.getHeight()
+    Height_Popculture = Popculture.getHeight()
+    Height_questionmark = questionmark.getHeight()
+
+    # Returns the center of the image
+    Center_food = food.getAnchor()
+    Center_animal = animal.getAnchor()
+    Center_countries = countries.getAnchor()
+    Center_Questionmark = questionmark.getAnchor()
+    Center_Popculture = Popculture.getAnchor()
+
+    # Returns the X Coord of the center of the image
+    X_Center_food = Center_food.getX()
+    X_Center_animal = Center_animal.getX()
+    X_Center_countries = Center_countries.getX()
+    X_Center_Questionmark = Center_Questionmark.getX()
+    X_Center_Popculture = Center_Popculture.getX()
+
+    # returns the Y cordinate of the center of the image
+    Y_Center_food = Center_food.getY()
+    Y_Center_animal = Center_animal.getY()
+    Y_Center_countries = Center_countries.getY()
+    Y_Center_Questionmark = Center_Questionmark.getY()
+    Y_Center_Popculture = Center_Popculture.getY()
+
+    # to get the max (X and Y) and min (X and Y) for the food
+    X_cordinate2_food = (Width_food/2) + X_Center_food
+    X_cordinate1_food = X_Center_food - (Width_food/2)
+    Y_cordinate2_food = (Height_food/2) + Y_Center_food
+    Y_cordinate1_food = Y_Center_food - (Height_food/2)
+
+    # to get the max (X and Y) and min (X and Y) for the animal
+    X_cordinate2_animal = (Width_animal/2) + X_Center_animal
+    X_cordinate1_animal = X_Center_animal - (Width_animal/2)
+    Y_cordinate2_animal = (Height_animal/2) + Y_Center_animal
+    Y_cordinate1_animal = Y_Center_animal - (Height_animal/2)
+
+    # to get the max (X and Y) and min (X and Y) for the countries
+    X_cordinate2_country = (Width_countries/2) + X_Center_countries
+    X_cordinate1_country = X_Center_countries - (Width_countries/2)
+    Y_cordinate2_country = (Height_countries/2) + Y_Center_countries
+    Y_cordinate1_country = Y_Center_countries - (Height_countries/2)
+
+    # to get the max (X and Y) and min (X and Y) for the PopCulture
+    X_cordinate2_PopCulture = (Width_Popculture/2) + X_Center_Popculture
+    X_cordinate1_PopCulture = X_Center_Popculture - (Width_Popculture/2)
+    Y_cordinate2_PopCulture = (Height_Popculture/2) + Y_Center_Popculture
+    Y_cordinate1_PopCulture = Y_Center_Popculture - (Height_Popculture/2)
+
+    # to get the max (X and Y) and min (X and Y) for the Question mark
+    X_cordinate2_questionmark = (Width_questionmark/2) + X_Center_Questionmark
+    X_cordinate1_questionmark = X_Center_Questionmark - (Width_questionmark/2)
+    Y_cordinate2_questionmark = (Height_questionmark/2) + Y_Center_Questionmark
+    Y_cordinate1_questionmark = Y_Center_Questionmark - (Height_questionmark/2)
+
+    # Drawing the window and all the images onto the window
+    PickCategory.draw(win)
+    food.draw(win)
+    animal.draw(win)
+    countries.draw(win)
+    Popculture.draw(win)
+    questionmark.draw(win)
+
+    # Returns the point where the mouse is clicked
+    Pos_mouse_clicked = win.getMouse()
+
+    # X-Position of the point clicked
+    X_point_mouse_clicked = Pos_mouse_clicked.getX()
+
+    # Y-Position of the point clicked
+    Y_point_mouse_clicked = Pos_mouse_clicked.getY()
+
+    # finds where the mouse has been clicked
+    # Food category
+    if X_point_mouse_clicked <  X_cordinate2_food and Y_cordinate1_PopCulture < Y_point_mouse_clicked < Y_cordinate2_PopCulture:
+        file = open("food.txt". "r")
+        word_list = file.readlines()
+        file.close()
+        word_to_guess = random.choice(word_list)
+
+        if ("\n" in word_to_guess):
+            word = word_to_guess
+        else:
+            word = word_to_guess
+
+        return word
+    # Animal category
+    elif X_cordinate1_animal < X_point_mouse_clicked < X_cordinate2_animal and Y_cordinate1_PopCulture < Y_point_mouse_clicked < Y_cordinate2_PopCulture:
+        file = open("animal.txt". "r")
+        word_list = file.readlines()
+        file.close()
+        word_to_guess = random.choice(word_list)
+
+        if ("\n" in word_to_guess):
+            word = word_to_guess
+        else:
+            word = word_to_guess
+
+        return word
+    # Geography category
+    elif X_cordinate1_country < X_point_mouse_clicked < X_cordinate2_country and Y_cordinate1_PopCulture < Y_point_mouse_clicked < Y_cordinate2_PopCulture:
+        file = open("geography.txt". "r")
+        word_list = file.readlines()
+        file.close()
+        word_to_guess = random.choice(word_list)
+
+        if ("\n" in word_to_guess):
+            word = word_to_guess
+        else:
+            word = word_to_guess
+
+        return word
+    # Pop culture
+    elif X_cordinate1_PopCulture < X_point_mouse_clicked < X_cordinate2_PopCulture and Y_cordinate1_PopCulture < Y_point_mouse_clicked < Y_cordinate2_PopCulture:
+        file = open("popculture.txt". "r")
+        word_list = file.readlines()
+        file.close()
+        word_to_guess = random.choice(word_list)
+
+        if ("\n" in word_to_guess):
+            word = word_to_guess
+        else:
+            word = word_to_guess
+
+        return word
+    # Random
+    elif X_cordinate1_questionmark < X_point_mouse_clicked < X_cordinate2_questionmark and Y_cordinate1_PopCulture < Y_point_mouse_clicked < Y_cordinate2_PopCulture:
+        categ_list = ['food.txt',
+                      'animal.txt',
+                      'geography.txt',
+                      'popculture.txt']
+        categ = random.choice(categ_list)
+
+        file = open(categ, 'r')
+        word_list = file.readlines()
+        file.close()
+
+        if ("\n" in word_to_guess):
+            word = word_to_guess.strip("\n")
+        else:
+            word = word_to_guess
+
+        return word
+    # The Y coordinate of the centers of the images is the same. I will use the
+    # height of the popculture image as my boundary for the click point because
+    # it has the greatest height "333" compared to the other images.
+    elif Y_cordinate1_PopCulture > Y_point_mouse_clicked or Y_point_mouse_clicked > Y_cordinate2_PopCulture:
+        print("invalid input, try clicking on the image.")
 
 
 # Create winner splash screen
@@ -128,15 +303,8 @@ def loserScreen():
     loserScreen.close()
 
 
-def choose_category():
-    """Screen where user choose the topic of the word."""
-    categoryWin = GraphWin("Hangman", WIDTH, HEIGHT)
-    food_image = Image(Point(100, 100), "food.PPM")
-    food_image.draw(categoryWin)
-
-
 # Main game window
-def gameWin():
+def gameWin(word):
     """Main game window."""
     guessWin = GraphWin("Hangman", WIDTH, HEIGHT)
 
@@ -151,74 +319,36 @@ def gameWin():
     line3.draw(guessWin)
     line4.draw(guessWin)
 
-    # Select from category file
-    # food = open('food.txt', 'r').readlines()
-    # word = random.choice(food)
+    # Draw dashes
 
     # Create user input box
     user_guess_box = Entry(Point(250, 400), 5)
     user_guess_box.draw(guessWin)
-    while True:
-        continue
-    while (user_word.chance_count < 7 and user_word.word_guess is not True):
-        user_word.choose_guess()
-        if (user_word.word_guess is False):
-            # If user has 0 wrong guess, print the head
-            if user_word.chance_count == 0:
-                user_word.chance_count += 1
-                head = Circle(Point(45,95),15)
-                head.draw(guessWin)
-            # If user has 1 wrong guess, print the body
-            elif user_word.chance_count == 1:
-                user_word.chance_count += 1
-                body = Line(Point(45, 110), Point(45, 150))
-                body.draw(guessWin)
-            # If user has 2 wrong guess, print the left leg
-            elif user_word.chance_count == 2:
-                    l_leg = Line(Point(45, 150), Point(25, 180))
-                    l_leg.draw(guessWin)
-            # If user has 3 wrong guess, print the right leg.
-            elif user_word.chance_count == 3:
-                    r_leg = Line(Point(45, 150), Point(65, 180))
-                    r_leg.draw(guessWin)
-            # If user has 4 wrong guess, print the left arm.
-            elif user_word.chance_count == 4:
-                    l_arm = Line(Point(45, 120), Point(25, 140))
-                    l_arm.draw(guessWin)
-            # If user has 5 wrong guess, print the right arm.
-            elif user_word.chance_count == 5:
-                r_arm = Line(Point(45, 120), Point(64, 140))
-                r_arm.draw(guessWin)
-            # If user has 6 wrong guess, print the left l_eye.
-            elif user_word.chance_count == 6:
-                l_eye_x = Line(Point(38, 90), Point(42, 94))
-                l_eye_x2 = Line(Point(42, 90), Point(38, 94))
-                l_eye_x.draw(guessWin)
-                l_eye_x2.draw(guessWin)
-            # If user has 7 wrong guess, print the right eye.
-            elif user_word.chance_count == 7:
-                r_eye_x = Line(Point(48, 90), Point(52, 94))
-                r_eye_x2 = Line(Point(52,90), Point(48, 94))
-                r_eye_x.draw(guessWin)
-                r_eye_x2.draw(guessWin)
-            # If the user has 8 wrong guess, print the mouth.
-            elif user_word.chance_count == 8:
-                mouth = Line(Point(42, 100), Point(47, 100))
-                mouth.draw(guessWin)
-           for i in range(len(user_word.guessed_letters)):
-               print(user_word.guessed_letters[i], end = " ")
-        else:
-            print("Congratulations! You won the game!")
 
-    print("Game over! The answer is " + word)
+    game_won = False
+
+    # While user has 8 or less incorrect guess or word has not been guessed
+    while ((user_word.chance_count <= 8) or (user_word.word_guess is not True)):
+        user_word.choose_guess()
+        # If word has not been guess
+        if (user_word.word_guess is False):
+            continue
+        else:
+            winnerScreen()  # Congratulations you won screen
+
+    loserScreen()
+
 
 gameWin()
-def test():
+
+
+def main():
     # Print welcome splash screen
     welcomeScreen()
 
     # Category screen
+    word = CategoryScreen()
+    user_word = Hangman(word)
 
     # Game screen
-
-    # Win or lose screen
+    gameWin(user_word)
